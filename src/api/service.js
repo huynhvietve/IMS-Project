cbbdb69f39035a93d0e4e5e544b1be04f747d6a2
Bien <duongquocbien2017@gmail.com>
2022-04-04 09:17:37 +0700

    Api

--- src/api/service.js
+++ src/api/service.js
@@ -1,8 +1,19 @@
 import { API_BASE } from "./config";
 import api from "./instance";
+import axios from "axios";
+import * as Config from "./config";
 export const loginAPI = (username, password) => {
   return api.post(`${API_BASE}/login`, {
     username: username,
     password: password,
   });
 };
+export function  CallAPI(endpoint, method = "GET", body) {
+  return axios({
+    method: method,
+    url: `${Config.API_URL}/${endpoint}`,
+    data: body,
+  }).catch((e) => {
+    console.error("lỗi kết nối");
+  });
+}
