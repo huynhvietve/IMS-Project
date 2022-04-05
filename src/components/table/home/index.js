6e12e12560731dd1affb2dc27174b40357f04a89
Bien <duongquocbien2017@gmail.com>
2022-04-05 16:41:37 +0700

    batch

--- src/components/table/home/index.js
+++ src/components/table/home/index.js
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import React, {useState, useEffect } from "react";
+import api from "../../../api/instance";
 export default function Home() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const [idcourse, setIdcourse] = useState([]);
+ 
+   useEffect( () => {
+    api.get(`http://192.168.43.74:5000/internshipcourse/batch${urlParams.get("id")}`, null )
+    .then( (res) => {
+      setIdcourse(res.data)
+      });
+}, []);
   return (
-    <h1>Chào mừng bạn đến với Batch 9</h1>
+    <>
+    {idcourse?.map((idcourse) => (
+  <div>
+    <h1>Chào mừng bạn đến với {idcourse.nameCoure}</h1>
+  </div>
+    ))}
+    </>
   )
 }
