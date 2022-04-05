eeb44d5112a1ea5fd3050a564d7cb42a371bc971
huuchanh12 <huuchanh2308@gmail.com>
2022-04-05 13:39:55 +0700

    index

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,7 @@
 import { API_BASE } from "./config";
 import api from "./instance";
 export const loginAPI = (username, password) => {
-  return api.post(`${API_BASE}/login`, {
+  return api.post(`${API_BASE}/auth/login`, {
     username: username,
     password: password,
   });
