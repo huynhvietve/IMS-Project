fa8e2b9bc96af2c536694c2dc50beddb1acbddcd
Bien <duongquocbien2017@gmail.com>
2022-04-06 10:04:24 +0700

    chuyen API sang service

--- src/api/service.js
+++ src/api/service.js
@@ -9,6 +9,9 @@ export const loginAPI = (username, password) => {
 export function batchAPI( endpoint)  {
   return api.get(`${API_BASE}/${endpoint}`,null);
 };
+export function batchHome( endpoint)  {
+  return api.get(`${API_BASE}/${endpoint}`,null);
+};
 export function batchCreate( endpoint, body)  {
   return api.post(`${API_BASE}/${endpoint}`,body);
 };
--- src/components/table/home/index.js
+++ src/components/table/home/index.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect } from "react";
-import api from "../../../api/instance";
+import * as apiaxios from "../../../api/service";
 export default function Home() {
   const urlParams = new URLSearchParams(window.location.search);
   const [idcourse, setIdcourse] = useState([]);
  
    useEffect( () => {
-    api.get(`http://192.168.43.74:5000/internshipcourse/batch${urlParams.get("id")}`, null )
+    apiaxios.batchHome(`internshipcourse/batch${urlParams.get("id")}`, null )
     .then( (res) => {
       setIdcourse(res.data)
       });
