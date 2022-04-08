aa15742cf93bec0b05861fdc527af4ab1abe866f
unknown <a7ngoctu@gmail.com>
2022-04-08 14:31:41 +0700

    fix

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const api = axios.create();
 
 api.interceptors.request.use((request) => {
-  // add auth header with jwt if local storage exist token
   const token = localStorage.getItem("token");
   if (token) {
     request.headers["Authorization"] = `Bearer ${token}`;
