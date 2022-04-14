dba04643bdc945b5abb508cfff3c33e16ef073c2
unknown <a7ngoctu@gmail.com>
2022-04-14 15:02:16 +0700

    fix bug dư

--- src/components/table/home/index.js
+++ src/components/table/home/index.js
@@ -5,7 +5,6 @@ import {useLocation} from "react-router-dom";
 export default function Home() {
   const urlParams = new URLSearchParams(window.location.search);
   const [idcourse, setIdcourse] = useState([]);
-  const location = useLocation();
    useEffect( () => {
     apiaxios.batchHome(`internshipcourse/${urlParams.get("id")}`, null )
     .then( (res) => {
