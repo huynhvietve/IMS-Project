eef8d98aaa27d59997590257c7bbfdb8cde659c8
unknown <a7ngoctu@gmail.com>
2022-04-14 15:04:35 +0700

    fix bug hoàn thành

--- src/components/table/home/index.js
+++ src/components/table/home/index.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect } from "react";
 import * as apiaxios from "../../../api/service";
-import {useLocation} from "react-router-dom";
 
 export default function Home() {
   const urlParams = new URLSearchParams(window.location.search);
